8ca68ff3217b536dc782cd70de7988a3d096ca90
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2014-07-24 00:09:57 +0200

    Implemented with Java8 stream

--- src/main/java/pl/rwieckowski/demo/jbehave/ViewShoppingListsUseCase.java
+++ src/main/java/pl/rwieckowski/demo/jbehave/ViewShoppingListsUseCase.java
@@ -1,35 +1,21 @@
 package pl.rwieckowski.demo.jbehave;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ViewShoppingListsUseCase {
     public List<ShoppingListSummary> viewShoppingLists(User user) {
-        List<Access> userAccesses = Context.accessRepository.findAccessesFor(user);
-        List<ShoppingList> shoppingLists = new ArrayList<ShoppingList>();
-        for (Access userAccess : userAccesses) {
-            ShoppingList shoppingList = userAccess.getShoppingList();
-            if (!shoppingList.isArchived()) {
-                shoppingLists.add(shoppingList);
-            }
-        }
+        return Context.accessRepository.findAccessesFor(user).stream()
+                .map(Access::getShoppingList)
+                .filter(sl -> !sl.isArchived())
+                .sorted((sl1, sl2) -> sl2.getCreateDate().compareTo(sl1.getCreateDate()))
+                .map(this::makeSummary)
+                .collect(Collectors.toList());
+    }
 
-        ShoppingList[] sla = shoppingLists.toArray(new ShoppingList[shoppingLists.size()]);
-        Arrays.sort(sla, new Comparator<ShoppingList>() {
-            @Override
-            public int compare(ShoppingList o1, ShoppingList o2) {
-                return o2.getCreateDate().compareTo(o1.getCreateDate());
-            }
-        });
-
-        List<ShoppingListSummary> sls = new ArrayList<ShoppingListSummary>();
-        for (ShoppingList sl : sla) {
-            ShoppingListSummary summary = new ShoppingListSummary();
-            summary.title = sl.getTitle();
-            sls.add(summary);
-        }
-        return sls;
+    private ShoppingListSummary makeSummary(ShoppingList shoppingList) {
+        ShoppingListSummary summary = new ShoppingListSummary();
+        summary.title = shoppingList.getTitle();
+        return summary;
     }
 }
