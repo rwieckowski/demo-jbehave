7c22171d44c951a24093605be6631e4a4c2be117
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2014-07-24 00:09:18 +0200

    Added story reporter

--- src/test/java/pl/rwieckowski/demo/jbehave/ShoppingLists.java
+++ src/test/java/pl/rwieckowski/demo/jbehave/ShoppingLists.java
@@ -4,6 +4,7 @@ import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.junit.JUnitStory;
+import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.junit.runner.RunWith;
@@ -13,7 +14,8 @@ import pl.rwieckowski.demo.jbehave.steps.ShoppingListsSteps;
 public class ShoppingLists extends JUnitStory {
     @Override
     public Configuration configuration() {
-        return new MostUsefulConfiguration();
+        return new MostUsefulConfiguration()
+                .useStoryReporterBuilder(new StoryReporterBuilder().withDefaultFormats());
     }
 
     @Override
