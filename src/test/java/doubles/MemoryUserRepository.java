ffaf989792ac5a363029ae1bd6595145ab64f632
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2014-07-24 00:08:43 +0200

    Diamond operator

--- src/test/java/doubles/MemoryUserRepository.java
+++ src/test/java/doubles/MemoryUserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class MemoryUserRepository implements UserRepository {
-    private List<User> users = new ArrayList<User>();
+    private List<User> users = new ArrayList<>();
 
     @Override
     public User findUserByName(String userName) {
